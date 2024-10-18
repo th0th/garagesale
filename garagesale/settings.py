6151f392337659306d7665790ba777b6652d596c
iibarbari <45620986+iibarbari@users.noreply.github.com>
2024-10-18 14:13:29 +0300

    hehe it works

--- garagesale/settings.py
+++ garagesale/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'garagesale.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- market/views.py
+++ market/views.py
@@ -4,9 +4,9 @@ from .models import Item
 # Create your views here.
 def list_view(request):
     items = Item.objects.all()
-    return render(request, 'items/list.html', {'items': items});
+    return render(request, 'market/list.html', {'items': items});
 
 
 def detail_view(request, id):
     item = Item.objects.get(id=id)
-    return render(request, 'items/detail.html', {'item': item});
+    return render(request, 'market/detail.html', {'item': item});
